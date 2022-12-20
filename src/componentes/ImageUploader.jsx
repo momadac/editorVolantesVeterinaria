47b4d418ac2d8330503020d415d16fc445d437e4
momadac <diegocastrodearg@gmail.com>
2022-12-20 17:37:44 -0300

    emprolijado sin guía

--- src/componentes/ImageUploader.jsx
+++ src/componentes/ImageUploader.jsx
@@ -2,16 +2,13 @@ import React, { useState, useEffect , useContext } from 'react';
 import { saveAs } from 'file-saver';
 import { GeneralContext } from "../contexts/GeneralContext"
 
-
 function ImageUploader() {
     const [imageUrl, setImageUrl] = useState(null);
     const {setTexto1 , setTexto2,seturlFoto,setTamTexto1 , setPosIniPalabra1X , setPosIniPalabra1Y , posIniPalabra1X , posIniPalabra1Y ,colorTexto1 , setColorTexto1,
         setColorTexto2, setbordeColorTexto1} = useContext(GeneralContext);
 
-    const handleFileChange = (event) => {
-        const file = event.target.files[0];
-        const fileUrl = URL.createObjectURL(file);
-        seturlFoto(fileUrl);
+    const handleFileChange = () => {
+        document.getElementById("hiddenFileInput").click();
     };
 
     const handleSave = () => {
@@ -20,12 +17,13 @@ function ImageUploader() {
 
     return (
         <div className="p-3">
-            <div class="input-group custom-file-button">
-            <input type="file" className="btn btn-primary btn-lg btn-block" id="inputGroupFile" onChange={handleFileChange} />
+        <div class="input-group custom-file-button">
+            <button type="button" className="btn btn-primary btn-lg btn-block" onClick={handleFileChange}>Seleccionar imágen</button>
+            <input type="file" className="d-none" id="hiddenFileInput" onChange={handleFileChange} />
         </div>
         {imageUrl && (
-            <>
-            <img src={imageUrl} alt="Selected image" />
+        <>
+        <img src={imageUrl} alt="Selected image" />
             <button onClick={handleSave}>Save image</button>
             </>
         )}
